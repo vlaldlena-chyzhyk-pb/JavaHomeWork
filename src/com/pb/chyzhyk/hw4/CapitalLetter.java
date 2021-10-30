0101e384f310c4a1254f29d9c08ac3837c3516fa
Vladlena Chyzhyk <vladlena.chyzhyk@privatbank.ua>
2021-10-30 23:10:13 +0300

    init commit 1

--- src/com/pb/chyzhyk/hw4/CapitalLetter.java
+++ src/com/pb/chyzhyk/hw4/CapitalLetter.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class CapitalLetter {
+
     static void print (String in){
         System.out.println(in);
     }
@@ -11,8 +12,16 @@ public class CapitalLetter {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите строку");
         String in = scan.nextLine();
-        String result = in.toUpperCase();
-        System.out.println("Result: " + result);
+        char[] chars = in.toCharArray();
+
+        chars[0] = Character.toUpperCase(chars[0]);
+        for (int i = 0; i < chars.length - 1; i++) {
+            if (chars[i] == ' ') {
+                chars[i + 1] = Character.toUpperCase(chars[i + 1]);
+            }
+        }
+        in = new String(chars);
+        System.out.println(in);
     }
 
 }
