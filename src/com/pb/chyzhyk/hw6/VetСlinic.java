1bdf8122696dad82e5991edbcfaa0d2724ae64cb
Vladlena Chyzhyk <vladlena.chyzhyk@privatbank.ua>
2021-11-07 00:12:44 +0200

    init commit 1

--- src/com/pb/chyzhyk/hw6/VetСlinic.java
+++ src/com/pb/chyzhyk/hw6/VetСlinic.java
@@ -1,17 +1,16 @@
 package com.pb.chyzhyk.hw6;
 
+
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
 public class VetСlinic {
-    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException {
+    public static void main(String[] args) throws Exception{
         Animal[] animals = new Animal[3];
         animals[0] = new Cat();
         animals[1] = new Dog();
         animals[2] = new Horse();
 
-        String[] string = new String[animals.length];
 
 
         Veterinarian veterinar = new Veterinarian();
@@ -20,7 +19,6 @@ public class VetСlinic {
         System.out.println(Arrays.toString(clazz.getMethods()));
 
         Class veterinarClazz = Class.forName("com.pb.chyzhyk.hw6.Veterinarian");
-        Class veterinarianClazz = Veterinarian.class;
         Constructor constr = veterinarClazz.getConstructor(new Class[]{String.class});
         Object obj = constr.newInstance("Doctor Who");
         if (obj instanceof Veterinarian) {
